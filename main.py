2dfd8460f364abf45f5738482c2dd7daf65561c2
Sociu Daniel <danielsociu00@gmail.com>
2021-12-13 21:13:59 +0200

    testing gym env

--- main.py
+++ main.py
@@ -3,14 +3,19 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import gym
 
-def print_hi(name):
+def test():
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
-
+    env = gym.make('CarRacing-v0')
+    env.reset()
+    for _ in range(5000):
+        env.render()
+        env.step(env.action_space.sample())  # take a random action
+    env.close()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    test()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
