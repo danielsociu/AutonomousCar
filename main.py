5733b65a415eeee864bc6a818276b9cea7f6f9c1
Daniel Sociu <danielsociu00@gmail.com>
2022-01-14 19:44:39 +0200

    Added base environment

--- main.py
+++ main.py
@@ -3,16 +3,20 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+from gym.utils import seeding, EzPickle
+import environment
 import gym
+
 
 def test():
     # Use a breakpoint in the code line below to debug your script.
-    env = gym.make('CarRacing-v0')
+    env = environment.CarRacing()
     env.reset()
     for _ in range(5000):
         env.render()
         env.step(env.action_space.sample())  # take a random action
     env.close()
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
