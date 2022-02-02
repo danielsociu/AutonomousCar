1aa0e9b2b67a63380f735fca3d526a06ea8cae0d
Kira060200 <constantin.gab02@gmail.com>
2022-02-02 22:34:13 +0200

    process image frame from state

---
+++ image_processing.py
@@ -0,0 +1,12 @@
+import cv2
+
+
+def get_processed_image(state):
+    image = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
+    image = image[16:, 16:]
+    image = image[:-16, :-16].astype(float)
+    image /= 255.0
+    # image = cv2.resize(image, (72, 72))
+    cv2.imshow('image', image)
+    cv2.waitKey(0)
+    return image
